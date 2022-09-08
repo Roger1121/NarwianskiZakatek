cdb6094cc5dd83da6c5e83c682c0c15d20ac9a5d
Roger1121 <leszczynski_szymon@wp.pl>
2022-09-08 19:26:20 +0200

    Role Management

--- NarwianskiZakatek/Controllers/AdminController.cs
+++ NarwianskiZakatek/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NarwianskiZakatek.Data;
 
@@ -20,6 +21,34 @@ namespace NarwianskiZakatek.Controllers
             return _context.Users != null ?
                         View(_context.Users.ToList()) :
                         Problem("Entity set 'ApplicationDbContext.Users'  is null.");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddToRole(string role, string userName)
+        {
+            string userId = _context.Users.Where(u => u.UserName == userName).FirstOrDefault().Id;
+            string roleId = _context.Roles.Where(u => u.Name == role).FirstOrDefault().Id;
+            _context.UserRoles.Add(new IdentityUserRole<string>()
+            {
+                RoleId = roleId,
+                UserId = userId
+            });
+            _context.SaveChanges();
+            return RedirectToAction("Users");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult RemoveFromRole(string role, string userName)
+        {
+            string userId = _context.Users.Where(u => u.UserName == userName).FirstOrDefault().Id;
+            string roleId = _context.Roles.Where(u => u.Name == role).FirstOrDefault().Id;
+            _context.UserRoles.Remove(new IdentityUserRole<string>()
+            {
+                RoleId = roleId,
+                UserId = userId
+            });
+            _context.SaveChanges();
+            return RedirectToAction("Users");
         }
     }
 }
--- NarwianskiZakatek/Models/SeedData.cs
+++ NarwianskiZakatek/Models/SeedData.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NarwianskiZakatek.Data;
+using Npgsql;
 
 namespace NarwianskiZakatek.Models
 {
@@ -11,55 +12,118 @@ namespace NarwianskiZakatek.Models
             using (var context = new ApplicationDbContext(serviceProvider.GetRequiredService<
                     DbContextOptions<ApplicationDbContext>>()))
             {
-                if (!context.Roles.Any())
+                try
                 {
-                    context.Roles.Add(new IdentityRole()
+                    #region add roles
+                    if (!context.Roles.Any())
                     {
-                        Name = "User",
-                        NormalizedName = "USER"
-                    });
-                    context.Roles.Add(new IdentityRole()
+                        context.Roles.Add(new IdentityRole()
+                        {
+                            Name = "User",
+                            NormalizedName = "USER"
+                        });
+                        context.Roles.Add(new IdentityRole()
+                        {
+                            Name = "Employee",
+                            NormalizedName = "EMPLOYEE"
+                        });
+                        context.Roles.Add(new IdentityRole()
+                        {
+                            Name = "Admin",
+                            NormalizedName = "ADMIN"
+                        });
+                        context.SaveChanges();
+                    }
+                    #endregion
+
+                    #region add users
+                    if (!context.Users.Any())
                     {
-                        Name = "Employee",
-                        NormalizedName = "EMPLOYEE"
-                    });
-                    context.Roles.Add(new IdentityRole()
-                    {
-                        Name = "Admin",
-                        NormalizedName = "ADMIN"
-                    });
-                    context.SaveChanges();
+                        context.Users.Add(new AppUser()
+                        {
+                            UserName = "leszczynski_szymon@wp.pl",
+                            NormalizedUserName = "LESZCZYNSKI_SZYMON@WP.PL",
+                            Email = "leszczynski_szymon@wp.pl",
+                            NormalizedEmail = "LESZCZYNSKI_SZYMON@WP.PL",
+                            EmailConfirmed = true,
+                            PasswordHash = "AQAAAAEAACcQAAAAEDe9LsLy6xxzvfbsvrUzRSNUXRp0YwKuzvbAU02CnhAGhHgLOXMfaqShadKmkujA+A==",
+                            SecurityStamp = "GFG64WLSCBWK22CVGPPDMNJA4GOA6RPB",
+                            ConcurrencyStamp = "6d39cc48-1616-4c58-8001-c694eaeeb107",
+                            PhoneNumber = "123456789"
+                        });
+                        context.SaveChanges();
+                        string userId = context.Users.Where(r => r.UserName == "leszczynski_szymon@wp.pl").ToList().FirstOrDefault().Id;
+                        context.UserRoles.Add(new IdentityUserRole<string>()
+                        {
+                            UserId = userId,
+                            RoleId = context.Roles.Where(r => r.Name == "User").ToList().FirstOrDefault().Id
+                        });
+                        context.UserRoles.Add(new IdentityUserRole<string>()
+                        {
+                            UserId = userId,
+                            RoleId = context.Roles.Where(r => r.Name == "Employee").ToList().FirstOrDefault().Id
+                        });
+                        context.UserRoles.Add(new IdentityUserRole<string>()
+                        {
+                            UserId = userId,
+                            RoleId = context.Roles.Where(r => r.Name == "Admin").ToList().FirstOrDefault().Id
+                        });
+                        context.SaveChanges();
+
+                        context.Users.Add(new AppUser()
+                        {
+                            UserName = "employee@test.pl",
+                            NormalizedUserName = "EMPLOYEE@TEST.PL",
+                            Email = "employee@test.pl",
+                            NormalizedEmail = "EMPLOYEE@TEST.PL",
+                            EmailConfirmed = true,
+                            PasswordHash = "AQAAAAEAACcQAAAAEJwNo3gck8NrGarCI2t3yHCpEX2/wUppwQhQQBtI/IwNdKsCtzWkoaNJaGOR/MpxZg==",
+                            SecurityStamp = "N4S2GQNOKBGA477TJKQB7A4KYCT4OCQ3",
+                            ConcurrencyStamp = "ab642b36-c5e9-4c7a-b088-2015d606a8e0",
+                            PhoneNumber = "123456789"
+                        });
+                        context.SaveChanges();
+                        userId = context.Users.Where(r => r.UserName == "employee@test.pl").ToList().FirstOrDefault().Id;
+                        context.UserRoles.Add(new IdentityUserRole<string>()
+                        {
+                            UserId = userId,
+                            RoleId = context.Roles.Where(r => r.Name == "User").ToList().FirstOrDefault().Id
+                        });
+                        context.UserRoles.Add(new IdentityUserRole<string>()
+                        {
+                            UserId = userId,
+                            RoleId = context.Roles.Where(r => r.Name == "Employee").ToList().FirstOrDefault().Id
+                        });
+                        context.SaveChanges();
+
+                        context.Users.Add(new AppUser()
+                        {
+                            UserName = "user@test.pl",
+                            NormalizedUserName = "USER@TEST.PL",
+                            Email = "user@test.pl",
+                            NormalizedEmail = "USER@TEST.PL",
+                            EmailConfirmed = true,
+                            PasswordHash = "AQAAAAEAACcQAAAAELKBPXv8HNDhwzwO7eMO6WVzaKDAqnMvrPGcrMo11/wYuFO6ik4Xx31FdlrgKjm7yg==",
+                            SecurityStamp = "2GW4WYDZN5BGMA2K4N5IJ24W5HGGZC6G",
+                            ConcurrencyStamp = "3f8a113b-44b7-493d-be2e-c2542d1d007f",
+                            PhoneNumber = "123456789"
+                        });
+                        context.SaveChanges();
+                        userId = context.Users.Where(r => r.UserName == "user@test.pl").ToList().FirstOrDefault().Id;
+                        context.UserRoles.Add(new IdentityUserRole<string>()
+                        {
+                            UserId = userId,
+                            RoleId = context.Roles.Where(r => r.Name == "User").ToList().FirstOrDefault().Id
+                        });
+                        context.SaveChanges();
+                    }
+                    #endregion
+
+
                 }
-                if (!context.Users.Any())
+                catch (NpgsqlException ex)
                 {
-                    context.Users.Add(new AppUser()
-                    {
-                        UserName = "leszczynski_szymon@wp.pl",
-                        NormalizedUserName = "LESZCZYNSKI_SZYMON@WP.PL",
-                        Email = "leszczynski_szymon@wp.pl",
-                        NormalizedEmail = "LESZCZYNSKI_SZYMON@WP.PL",
-                        EmailConfirmed = true,
-                        PasswordHash = "AQAAAAEAACcQAAAAEDe9LsLy6xxzvfbsvrUzRSNUXRp0YwKuzvbAU02CnhAGhHgLOXMfaqShadKmkujA+A==",
-                        SecurityStamp = "GFG64WLSCBWK22CVGPPDMNJA4GOA6RPB",
-                        ConcurrencyStamp = "6d39cc48-1616-4c58-8001-c694eaeeb107",
-                        PhoneNumber = "123456789"
-                    });
-                    context.SaveChanges();
-                    string userId = context.Users.Where(r => r.UserName == "leszczynski_szymon@wp.pl").ToList().FirstOrDefault().Id;
-                    context.UserRoles.Add( new IdentityUserRole<string>()
-                    {
-                        UserId = userId,
-                        RoleId = context.Roles.Where(r => r.Name == "User").ToList().FirstOrDefault().Id
-                    });context.UserRoles.Add( new IdentityUserRole<string>()
-                    {
-                        UserId = userId,
-                        RoleId = context.Roles.Where(r => r.Name == "Employee").ToList().FirstOrDefault().Id
-                    });context.UserRoles.Add( new IdentityUserRole<string>()
-                    {
-                        UserId = userId,
-                        RoleId = context.Roles.Where(r => r.Name == "Admin").ToList().FirstOrDefault().Id
-                    });
-                    context.SaveChanges();
+                    return;
                 }
             }
         }
