703ddafcbd5c633a7bf20eaf783a21a900f49bef
Roger1121 <leszczynski_szymon@wp.pl>
2022-10-02 18:06:04 +0200

    working weather widget

---
+++ NarwianskiZakatek/Models/MailRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace NarwianskiZakatek.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public List<IFormFile> Attachments { get; set; } = new List<IFormFile>();
+    }
+}
