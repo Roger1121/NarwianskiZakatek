3423ba3beffae2bc49f5a1c91e6973f877fe0c9e
Roger1121 <leszczynski_szymon@wp.pl>
2022-10-30 13:13:50 +0100

    external login

--- NarwianskiZakatek/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
+++ NarwianskiZakatek/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -50,7 +50,7 @@ namespace NarwianskiZakatek.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "Hasło musi zawierać od {2} do {1} znaków.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Nowe hasło")]
             public string NewPassword { get; set; }
--- NarwianskiZakatek/Program.cs
+++ NarwianskiZakatek/Program.cs
@@ -5,21 +5,51 @@ using NarwianskiZakatek.Models;
 using NarwianskiZakatek.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Database config
+var connectionString = config.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// Identity config
 builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSingleton(builder.Configuration.GetSection("EmailConfig").Get<EmailConfig>());
+// SMTP config
+builder.Services.AddSingleton(config.GetSection("EmailConfig").Get<EmailConfig>());
 builder.Services.AddScoped<IEmailService, EmailService>();
 
+//External login config
+builder.Services.AddAuthentication()
+   .AddGoogle(options =>
+   {
+       IConfigurationSection googleAuthNSection =
+       config.GetSection("Authentication:Google");
+       options.ClientId = googleAuthNSection["ClientId"];
+       options.ClientSecret = googleAuthNSection["ClientSecret"];
+   })
+   .AddFacebook(options =>
+   {
+       IConfigurationSection FBAuthNSection =
+       config.GetSection("Authentication:Facebook");
+       options.ClientId = FBAuthNSection["AppId"];
+       options.ClientSecret = FBAuthNSection["AppSecret"];
+   })
+   .AddMicrosoftAccount(microsoftOptions =>
+   {
+       microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
+       microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
+   })
+   .AddTwitter(twitterOptions =>
+   {
+       twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
+       twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
+       twitterOptions.RetrieveUserDetails = true;
+   });
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 var app = builder.Build();
@@ -49,10 +79,10 @@ app.MapControllerRoute(
     pattern: "{controller=Posts}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+// Inicjalizacja bazy danych
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-
     SeedData.Initialize(services);
 }
 
